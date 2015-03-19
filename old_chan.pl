53ae224abfd3cb6952e0a8b45b6e13ad84e8ba76
Tim Bowers <djsmiley2k@gmail.com>
2015-03-19 23:17:59 +0000

    now loads in irssi

--- old_chan.pl
+++ old_chan.pl
@@ -1,38 +1,39 @@
-
-
+use strict;
+use warnings;
 
 use Irssi;
-%VERSION = '0.2';
-%IRSSI = (
+my %VERSION = ("0.3");
+my %IRSSI = (
 	authors => 'djsmiley2k',
 	contact => 'djsmiley2k@gmail.com',
 	name => 'Old Channel',
 	description => 'automatically splits out old channel window; closes older channel window and joins new channel',
 	license => 'GPL2',
 );
-my $curr_chan = Irssi::active_win();
 
+my $curr_chan = Irssi::active_win();
+my $old_chan = Irssi::active_win();
 
 sub push_old {
 	# Push $curr_chan to $old_chan
-	Irssi::command("/Window hide " $old_chan);
+	Irssi::command("/Window hide " . $old_chan);
 	my $old_chan = $curr_chan;
 }
 
 sub make_win {
 	# Make new window using $old_chan
 	# /window show $old_chan
-	Irssi::command("/WINDOW split " $old_chan);
+	Irssi::command("/WINDOW split " . $old_chan);
 }
 
 sub store_new {
 	# store new channel name in $curr_chan
-	my $curr_chan = Irssi::active_win();
+	$curr_chan = Irssi::active_win();
 	
 }
 
 Irssi::signal_add_first( 'window item changed', \&push_old);
 Irssi::signal_add_last( 'window item changed', \&make_win);
-Irssi::signal_add_last( 'window item changed' \&store_new);
+Irssi::signal_add_last( 'window item changed', \&store_new);
 
 
