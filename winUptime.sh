872f27a1af9f74daf8f4465c29b9d8acf6772ec2
Tim Bowers <djsmiley2k@gmail.com>
2016-03-15 10:01:51 +0000

    Update winUptime.sh

--- winUptime.sh
+++ winUptime.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Shamelessly borrowed from http://correctlife.blogspot.de/2011/02/wrapper-on-checkntuptime.html
-
 HOSTADDRESS=$1
 MAXWARN=$2 # in hours
 MAXCRIT=$3 # in hours
@@ -11,9 +9,14 @@ STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
 
-SECONDS=`/usr/local/nagios/libexec/check_nt -H $HOSTADDRESS -p 12489 -s $ekR3t $2 $3`
+SECONDS=`/usr/local/nagios/libexec/check_nt -H $HOSTADDRESS -p 12489 -s $ekr3t -v COUNTER -l "\\System\\System Up Time"`
 #### IS:           6817
 #### SHOULD BE:    WARNING: uptime: 1:53 < warning|'uptime'=6817000;172800000;3600000;
+
+if [[ $SECONDS == 0 ]]; then
+        echo "UNKNOWN: No uptime recieved."
+        exit 3
+fi
 
 HOURS=$(( $SECONDS / 60 / 60 ))
 SECONDSINHOURS=$(( $HOURS * 60 * 60 ))
@@ -21,6 +24,11 @@ DAYS=$(( $HOURS / 24 ))
 REMAININGSECONDS=$(( $SECONDS - $SECONDSINHOURS ))
 MINUTES=$(( $REMAININGSECONDS / 60 ))
 FORMEDUPTIME="${DAYS}"
+
+if [[ $MINUTES -le 60 ]]; then
+        echo "CRITICAL: System restarted in last hour."
+        exit 2
+fi
 
 if [[ $DAYS -ge $MAXCRIT ]]; then
     echo "CRITICAL: System up over ${MAXCRIT} days."
@@ -34,3 +42,7 @@ fi
 
 echo "OK. Uptime $FORMEDUPTIME Days."
 exit 0
+
+
+
+
